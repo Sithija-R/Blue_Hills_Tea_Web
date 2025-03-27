10814b90590a24e107386ef1395616c8303f924a
Sithija R <Sithijarathukorale@gmail.com>
2025-03-27 19:55:35 +0530

    initial commit

---
+++ blue_hills_tea/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+const App=()=> {
+  
+
+  return (
+    
+    <div className="h-screen bg-primary-light flex items-center justify-center">
+      <h1 className="font-semibold text-4xl  ">Blue Hills Tea</h1>
+    </div>
+  )
+}
+
+export default App
