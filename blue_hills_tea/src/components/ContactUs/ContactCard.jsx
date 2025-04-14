039f39a3f23b76b6e74729e1cf4456e2980f4765
Sithija R <Sithijarathukorale@gmail.com>
2025-04-14 08:19:03 +0530

    modified

--- blue_hills_tea/src/components/ContactUs/ContactCard.jsx
+++ blue_hills_tea/src/components/ContactUs/ContactCard.jsx
@@ -1,30 +1,36 @@
-
-
-import React from 'react';
-import { FiMapPin, FiPhoneCall, FiClock } from 'react-icons/fi';
-import './Contact.css';
+import React from "react";
+import { FiMapPin, FiPhoneCall, FiClock } from "react-icons/fi";
+import "./Contact.css";
 
 const ContactCard = () => {
   return (
     <div className="w-full flex flex-col px-4 md:px-12 py-12">
-      
-      <h2 className="text-2xl md:text-3xl font-bold mb-6 text-left">Contact Us</h2>
-      <p>We’d love to hear from you. Whether you have questions about our teas, wholesale inquiries, or feedback, our team is here to help.</p>
-      
+      <h2 className="text-2xl md:text-3xl font-bold mb-6 text-left">
+        Contact Us
+      </h2>
+      <p>
+        We’d love to hear from you. Whether you have questions about our teas,
+        wholesale inquiries, or feedback, our team is here to help.
+      </p>
+
       <div className="flex flex-col md:flex-row justify-center items-center gap-8 mt-8">
-        
         <div className="bg-blue-100 bg-opacity-10 backdrop-blur-lg p-6 rounded-xl shadow-md w-full md:w-1/3 text-left min-h-[275px] transform transition-all duration-200 hover:scale-105">
           <div className="text-3xl text-blue-500 mx-auto mb-4">
             <FiMapPin />
           </div>
           <h3 className="text-lg font-semibold mb-2">Visit Our Factory</h3>
-          <p className="text-sm">Blue Hill Tea Factory,<br />Bogawanthalawa,<br />Balangoda</p>
+          <p className="text-sm">
+            Blue Hill Tea Factory,
+            <br />
+            Bogawanthalawa,
+            <br />
+            Balangoda
+          </p>
           <button className="mt-10.5 bg-blue-500 hover:bg-blue-600 text-white py-1 px-4 rounded-md text-sm">
             Get Directions
           </button>
         </div>
 
-        
         <div className="bg-blue-100 bg-opacity-10 backdrop-blur-lg p-6 rounded-xl shadow-md w-full md:w-1/3 text-left min-h-[275px] transform transition-all duration-200 hover:scale-105">
           <div className="text-3xl text-blue-500 mx-auto mb-4">
             <FiPhoneCall />
@@ -40,7 +46,6 @@ const ContactCard = () => {
           </button>
         </div>
 
-        
         <div className="bg-blue-100 bg-opacity-10 backdrop-blur-lg p-6 rounded-xl shadow-md w-full md:w-1/3 text-left min-h-[275px] transform transition-all duration-200 hover:scale-105">
           <div className="text-3xl text-blue-500 mx-auto mb-4">
             <FiClock />
