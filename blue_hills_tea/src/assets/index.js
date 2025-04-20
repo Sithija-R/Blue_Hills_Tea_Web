c8d3e93687faacf1b3b761e5a1e568cc2ee3a38c
Sithija R <Sithijarathukorale@gmail.com>
2025-04-20 14:32:20 +0530

    hero development

--- blue_hills_tea/src/assets/index.js
+++ blue_hills_tea/src/assets/index.js
@@ -1,5 +1,6 @@
 import Slider_01 from './images/slider_01.jpeg';
 import PaginationActive from './images/paginationActive.svg';
 import PaginationInactive from './images/paginationInactive.svg';
+import Tea_Leave from './images/tea_leave.png';
 
-export { Slider_01, PaginationActive, PaginationInactive };  
+export { Slider_01, PaginationActive, PaginationInactive, Tea_Leave };  
--- blue_hills_tea/src/components/Hero.jsx
+++ blue_hills_tea/src/components/Hero.jsx
@@ -4,7 +4,12 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
-import { Slider_01, PaginationInactive, PaginationActive } from "../assets"; // Import icons
+import {
+  Slider_01,
+  PaginationInactive,
+  PaginationActive,
+  Tea_Leave,
+} from "../assets";
 import Navbar from "./Navbar";
 import { shortAbout } from "../contents";
 import { slideIn } from "../utils/motion";
@@ -33,6 +38,23 @@ const Hero = () => {
 
   const handleSlideChange = (swiper) => {
     setCurrentIndex(swiper.activeIndex);
+  };
+
+  const CatalogCard = () => {
+    return (
+      <motion.div
+        whileHover={{ scale: 1.05 }}
+        className="bg-white shadow-lg rounded-xl p-6 max-w-sm"
+      >
+        <img
+          src="https://www.bluelankatours.com/wp-content/uploads/2024/11/Pedro-Tea-Estate-Sri-Lanka.jpg"
+          alt="Tea"
+          className="w-full h-40 object-cover rounded-t-lg"
+        />
+        <h2 className="text-xl font-bold mt-4">Tea Name</h2>
+        <p className="text-gray-600 mt-2">Description of the tea.</p>
+      </motion.div>
+    );
   };
 
   return (
@@ -115,7 +137,25 @@ const Hero = () => {
       </section>
 
       {/* About Section */}
-      <section ref={ref} className="w-full h-screen mt-34">
+      <section ref={ref} className="w-full relative h-[70vh] mt-34 ">
+        <motion.img
+          src={Tea_Leave}
+          alt="Tea Leave"
+          className="absolute top-90 -left-14  w-40  rounded-2xl object-cover rotate-90"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: isInView ? 1 : 0 }}
+          transition={{ duration: 3 }}
+          whileHover={{ scale: 1.05, transition: { duration: 0.3 } }}
+        />
+        <motion.img
+          src={Tea_Leave}
+          alt="Tea Leave"
+          className="absolute -top-20 right-0 w-40  rounded-2xl object-cover "
+          initial={{ opacity: 0 }}
+          animate={{ opacity: isInView ? 1 : 0 }}
+          transition={{ duration: 3 }}
+          whileHover={{ scale: 1.05, transition: { duration: 0.3 } }}
+        />
         <div className="flex justify-evenly lg:flex-row flex-col-reverse gap-10 lg:px-20 px-8">
           {/* Image with Slide-in Animation */}
           <motion.div
@@ -123,7 +163,7 @@ const Hero = () => {
             initial="hidden"
             whileInView="show"
             viewport={{ once: true, amount: 0.5 }}
-            className="lg:flex-[0.95]  flex bg-black-100 lg:p-8 rounded-2xl"
+            className="lg:flex-[0.95]  flex bg-black-100 lg:p-8 rounded-2xl "
           >
             <motion.img
               src="https://www.bluelankatours.com/wp-content/uploads/2024/11/Pedro-Tea-Estate-Sri-Lanka.jpg"
@@ -180,6 +220,7 @@ const Hero = () => {
             <motion.div
               className="mt-8 flex items-center gap-2 justify-end px-8 cursor-pointer"
               whileHover={{ scale: 1.05 }}
+              style={{ transformOrigin: "right center" }}
             >
               <motion.p className=" text-[#2563EB] font-medium">
                 Read Full Story
@@ -190,6 +231,70 @@ const Hero = () => {
               />
             </motion.div>
           </motion.div>
+        </div>
+      </section>
+      <section className="w-full  h-full mt-34 px-10">
+        <motion.p className=" text-[#2563EB] font-medium text-xl">
+          Tea Catalogue
+        </motion.p>
+        <p className="mt-1 text-4xl font-medium font-league">
+          Explore Our Finest Tea Selection
+        </p>
+        <div>
+          <div className="px-15 mt-5 flex flex-wrap gap-15">
+            {[1, 1, 1, 1, 1, 1, 1, 1].map((_, index) => (
+              <CatalogCard />
+            ))}
+          </div>
+          <motion.div
+            className="mt-8 flex items-center gap-2 justify-end px-8 cursor-pointer"
+            whileHover={{ scale: 1.05 }}
+            style={{ transformOrigin: "right center" }} // ← anchor point here
+          >
+            <motion.p className="text-[#2563EB] font-medium">
+              Full Catalogue
+            </motion.p>
+            <FontAwesomeIcon
+              icon={faArrowUpRightFromSquare}
+              className="text-blue-600 ml-2"
+            />
+          </motion.div>
+        </div>
+      </section>
+      <section className="w-full relative h-full mt-34  bg-lime-100 flex justify-center">
+        <div className="px-40 py-20 bg-red-100 flex flex-col items-center">
+          <motion.p className="  font-semibold text-[36px] mb-20">
+            Present Tea Production
+          </motion.p>
+          <div className="flex justify-between gap-20">
+            <motion.p className="text-[52px] font-semibold text-center flex flex-col">
+              750,000kg
+              <span className="font-medium text-[16px] text-[#52525B]">
+                Annual Tea Production
+              </span>
+              <span className="font-light text-[14px] text-[#52525B]">
+                Crafted to perfection
+              </span>
+            </motion.p>
+            <motion.p className="text-[52px] font-semibold text-center flex flex-col">
+             15+
+              <span className="font-medium text-[16px] text-[#52525B]">
+              Market Reach
+              </span>
+              <span className="font-light text-[14px] text-[#52525B]">
+              Sri Lanka to the world
+              </span>
+            </motion.p>
+            <motion.p className="text-[52px] font-semibold text-center flex flex-col">
+            3000kg
+              <span className="font-medium text-[16px] text-[#52525B]">
+              Daily Processing Capacity
+              </span>
+              <span className="font-light text-[14px] text-[#52525B]">
+              Fresh leaves, finest brew
+              </span>
+            </motion.p>
+          </div>
         </div>
       </section>
     </div>
