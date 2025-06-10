d2f745b439bb5d90627c6b56fa4d1b368b63a8c6
Sithija R <Sithijarathukorale@gmail.com>
2025-06-10 19:44:55 +0530

    routing modifications

--- blue_hills_tea/src/main.jsx
+++ blue_hills_tea/src/main.jsx
@@ -1,29 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-// Get a reference to the preloader element from the HTML
-const preloader = document.getElementById('preloader');
-
-// Function to hide the preloader
-const hidePreloader = () => {
-  if (preloader) {
-    preloader.style.opacity = '0'; // Start the fade-out effect
-    // Remove the preloader from the DOM after the transition completes
-    setTimeout(() => {
-      preloader.style.display = 'none';
-      preloader.remove(); // Clean up the DOM
-    }, 500); // This duration should match the CSS transition time (0.5s)
-  }
-};
-
-// Render your React application
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-);
-
-
-hidePreloader();
+)
